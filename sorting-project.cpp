40fd46c7e59a6fd1c0fffd35fa8f9b8c6e137b21
Nick Greiner <nicholas.greiner@hotmail.com>
2020-09-22 18:47:21 -0400

    Create sorting-project.cpp

---
+++ sorting-project.cpp
@@ -0,0 +1,30 @@
+/*
+Names:  Nick Greiner, Matt Ray, Brysen Allen, Sam Mycroft
+Class:  CSC-2710-01
+Date:   9/22/2020
+
+Description:  Program to to measure the time and number of basic
+              operations (compares) different algorithms take to sort
+              different datasets.
+
+https://github.com/NickGreiner/CSC-2710-Sorting-Project
+*/
+
+#include<stdio>
+#include<stdlib.h>
+#include<vector>
+
+using namespace std;
+
+void selectionSort();
+void bubbleSort();
+void insertionSort();
+void mergeSort();
+void quickSort();
+void heapSort();
+
+void loadDataset(vector<int> &dataset);
+
+int main() {
+
+}
