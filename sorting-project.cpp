cfc1746aa253fce77768bd7a0b33a47831738e9f
Nick Greiner <nicholas.greiner@hotmail.com>
2020-09-22 21:53:35 -0400

    Implemented main() function

--- sorting-project.cpp
+++ sorting-project.cpp
@@ -13,8 +13,10 @@ https://github.com/NickGreiner/CSC-2710-Sorting-Project
 
 #include<iostream>
 #include<vector>
+#include<chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 void selectionSort(vector<int> &dataset);
 void bubbleSort(vector<int> &dataset);
@@ -23,8 +25,58 @@ void mergeSort(vector<int> &dataset);
 void quickSort(vector<int> &dataset);
 void heapSort(vector<int> &dataset);
 
-void loadDataset(vector<int> &dataset);
+vector<int> loadDataset(string datafile);
+
+void runTimeTest(vector<int> &dataset, int algoNum);
 
 int main() {
 
+  string datafile;
+  vector<int> dataset;
+
+  // Almost Sorted
+  cout << "Enter almost sorted dataset: ";
+  cin >> datafile;
+  cout << endl;
+
+  dataset = loadDataset(datafile);
+
+  for (int x = 0; x < 6; x++){
+    runTimeTest(dataset, x);
+  }
+
+  // Randomly Distributed
+  cout << "Enter randomly distributed dataset: ";
+  cin >> datafile;
+  cout << endl;
+
+  dataset = loadDataset(datafile);
+
+  for (int x = 0; x < 6; x++){
+    runTimeTest(dataset, x);
+  }
+
+  // Reverse Sorted
+  cout << "Enter reverse sorted dataset: ";
+  cin >> datafile;
+  cout << endl;
+
+  dataset = loadDataset(datafile);
+
+  for (int x = 0; x < 6; x++){
+    runTimeTest(dataset, x);
+  }
+
+  // Duplicates
+  cout << "Enter duplicates dataset: ";
+  cin >> datafile;
+  cout << endl;
+
+  dataset = loadDataset(datafile);
+
+  for (int x = 0; x < 6; x++){
+    runTimeTest(dataset, x);
+  }
+
+  return 0;
 }
