4be5b516655fe128e0102b5ef122c427c80bec51
Nick Greiner <nicholas.greiner@hotmail.com>
2020-09-23 15:07:03 -0400

    Implemented selectionSort()

--- sorting-project.cpp
+++ sorting-project.cpp
@@ -100,6 +100,18 @@ int main() {
 
 void selectionSort(vector<int> &dataset) {
   vector<int> testSet = dataset;
+
+  int n = testSet.size();
+  int i, j, min_idx;
+
+  for (i = 0; i < n-1; i++) {
+    min_idx = i;
+    for (j = i+1; j < n; j++)
+    if (testSet[j] < testSet[min_idx])
+        min_idx = j;
+
+    swap(testSet[min_idx], testSet[i]);
+  }
 }
 
 void bubbleSort(vector<int> &dataset) {
